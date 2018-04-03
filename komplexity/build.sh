59fe731583e63997152a37f221997d0fede891bc
Erik <ecl@mail.med.upenn.edu>
2018-04-03 16:25:15 -0400

    pulls static binary from eclarke/komplexity

--- komplexity/build.sh
+++ komplexity/build.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
 
-# Use musl to build a non-dynamic binary (does not rely on glibc)
-rustup target add x86_64-unknown-linux-musl
-cargo build --release --target x86_64-unknown-linux-musl
-cp target/x86_64-unknown-linux-musl/release/kz $PREFIX/bin/kz
-
-#from rust-bio-tools recipe (https://github.com/bioconda/bioconda-recipes/blob/master/recipes/rust-bio-tools/build.sh)
-#C_INCLUDE_PATH=$PREFIX/include LIBRARY_PATH=$PREFIX/lib cargo install --root $PREFIX
+# Copy the static binary to the path
+cp kz $PREFIX/bin/kz
 
