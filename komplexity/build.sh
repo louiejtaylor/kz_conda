4687c0e32de33b49f081e7d0c54dbc139aa6e89c
Erik Clarke <ecl@mail.med.upenn.edu>
2018-04-03 12:53:34 -0400

    [nonworking] musl targets

--- komplexity/build.sh
+++ komplexity/build.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+# Use musl to build a non-dynamic binary (does not rely on glibc)
+rustup target add x86_64-unknown-linux-musl
+cargo build --release --target x86_64-unknown-linux-musl
+cp target/x86_64-unknown-linux-musl/release/kz $PREFIX/bin/kz
+
 #from rust-bio-tools recipe (https://github.com/bioconda/bioconda-recipes/blob/master/recipes/rust-bio-tools/build.sh)
-C_INCLUDE_PATH=$PREFIX/include LIBRARY_PATH=$PREFIX/lib cargo install --root $PREFIX
+#C_INCLUDE_PATH=$PREFIX/include LIBRARY_PATH=$PREFIX/lib cargo install --root $PREFIX
 
